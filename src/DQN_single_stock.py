7b80ebb803638366b27f6ea21ca1f4c724b9cbca
Bijayan Ray <94698348+BijayanRay@users.noreply.github.com>
2024-09-07 19:50:22 +0530

    Add files via upload

---
+++ src/DQN_single_stock.py
@@ -0,0 +1,329 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import time
+import copy
+import numpy as np
+import pandas as pd
+import chainer
+import chainer.functions as F
+import chainer.links as L
+from plotly import tools
+from plotly.graph_objs import *
+from plotly.offline import init_notebook_mode, iplot, iplot_mpl
+import matplotlib.pyplot as plt
+init_notebook_mode()
+
+data = pd.read_csv('GOOG.csv')
+data['Date'] = pd.to_datetime(data['Date'])
+data = data.set_index('Date')
+print(data.index.min(), data.index.max())
+data.head()
+
+date_split = '2005-01-01'
+train = data['2020-01-01':'2023-01-01']
+test = data['2023-01-02':'2024-03-22']
+len(train), len(test)
+
+def plot_train_test(train, test, date_split):
+
+    data = [
+        Candlestick(x=train.index, open=train['Open'], high=train['High'], low=train['Low'], close=train['Close'], name='train'),
+        Candlestick(x=test.index, open=test['Open'], high=test['High'], low=test['Low'], close=test['Close'], name='test')
+    ]
+    layout = {
+         'shapes': [
+             {'x0': date_split, 'x1': date_split, 'y0': 0, 'y1': 1, 'xref': 'x', 'yref': 'paper', 'line': {'color': 'rgb(0,0,0)', 'width': 1}}
+         ],
+        'annotations': [
+            {'x': date_split, 'y': 1.0, 'xref': 'x', 'yref': 'paper', 'showarrow': False, 'xanchor': 'left', 'text': ' test data'},
+            {'x': date_split, 'y': 1.0, 'xref': 'x', 'yref': 'paper', 'showarrow': False, 'xanchor': 'right', 'text': 'train data '}
+        ]
+    }
+    figure = Figure(data=data, layout=layout)
+    iplot(figure)
+
+plot_train_test(train, test, date_split)
+
+class Environment1:
+
+    def __init__(self, data, history_t=90):
+        self.data = data
+        self.history_t = history_t
+        self.reset()
+
+# Each state is in the form of an array
+# self.position holds the history of the buys with the prices
+# self.position_value gives the total price on selling all the stocks at a certain time
+# self.history holds the record for the difference in the consecutive stock prices over 90 days
+# reset provides the array of history concatenated with self.position_value
+# In this scenario, we will be selling all the stocks when the action is sell.
+
+    def reset(self):
+        self.t = 0
+        self.done = False
+        self.profits = 0
+        self.positions = []
+        self.position_value = 0
+        self.history = [0 for _ in range(self.history_t)]
+        return [self.position_value] + self.history # obs
+
+#step returns the state which is the concatenation of self.positon_value along with the history, along with reward.
+
+    def step(self, act):
+        reward = 0
+
+        # act = 0: stay, 1: buy, 2: sell
+        if act == 1:
+            self.positions.append(self.data.iloc[self.t, :]['Close'])
+        elif act == 2: # sell
+            if len(self.positions) == 0:
+                reward = -1
+            else:
+                profits = 0
+                for p in self.positions:
+                    profits += (self.data.iloc[self.t, :]['Close'] - p)
+                reward += profits
+                self.profits += profits
+                self.positions = []
+
+        # set next time
+        self.t += 1
+        self.position_value = 0
+        for p in self.positions:
+            self.position_value += (self.data.iloc[self.t, :]['Close'] - p)
+        self.history.pop(0)
+        self.history.append(self.data.iloc[self.t, :]['Close'] - self.data.iloc[(self.t-1), :]['Close'])
+
+        # clipping reward
+        if reward > 0:
+            reward = 1
+        elif reward < 0:
+            reward = -1
+
+        return [self.position_value] + self.history, reward, self.done # obs, reward, done
+
+env = Environment1(train)
+print(env.reset())
+for _ in range(3):
+    pact = np.random.randint(3)
+    print(env.step(pact))
+
+# DQN training
+
+# DQN
+
+def train_dqn(env):
+
+    class Q_Network(chainer.Chain):
+
+        def __init__(self, input_size, hidden_size, output_size):
+            super(Q_Network, self).__init__(
+                fc1 = L.Linear(input_size, hidden_size),
+                fc2 = L.Linear(hidden_size, hidden_size),
+                fc3 = L.Linear(hidden_size, output_size)
+            )
+
+        def __call__(self, x):
+            h = F.relu(self.fc1(x))
+            h = F.relu(self.fc2(h))
+            y = self.fc3(h)
+            return y
+
+        def reset(self):
+            self.zerograds()
+
+    Q = Q_Network(input_size=env.history_t+1, hidden_size=100, output_size=3) # Original network
+    Q_ast = copy.deepcopy(Q) # Target network
+    optimizer = chainer.optimizers.Adam()
+    optimizer.setup(Q)
+
+    epoch_num = 50
+    step_max = len(env.data)-1
+    memory_size = 200
+    batch_size = 20
+    epsilon = 1.0
+    epsilon_decrease = 1e-3
+    epsilon_min = 0.1
+    start_reduce_epsilon = 200
+    train_freq = 10
+    update_q_freq = 20
+    gamma = 0.97
+    show_log_freq = 5
+
+    memory = []
+    total_step = 0
+    total_rewards = []
+    total_losses = []
+
+    start = time.time()
+    for epoch in range(epoch_num):
+
+        pobs = env.reset() # pobs is of the form [position_value]+history
+        step = 0
+        done = False
+        total_reward = 0
+        total_loss = 0
+
+        while not done and step < step_max:
+
+            # select action epsilon greedily
+            pact = np.random.randint(3)
+            if np.random.rand() > epsilon:
+                pact = Q(np.array(pobs, dtype=np.float32).reshape(1, -1))
+                pact = np.argmax(pact.data)
+
+            # situation after taking the action including the reward and the observation
+            obs, reward, done = env.step(pact)
+
+            # in the memory, add the observations, the action taken at that observation along with the reward recieved on taking the action.
+            memory.append((pobs, pact, reward, obs, done))
+            if len(memory) > memory_size:
+                memory.pop(0)
+
+            # train or update q via batch sampling.
+            if len(memory) == memory_size:
+                if total_step % train_freq == 0:
+                    shuffled_memory = np.random.permutation(memory)
+                    memory_idx = range(len(shuffled_memory))
+                    for i in memory_idx[::batch_size]:
+                        batch = np.array(shuffled_memory[i:i+batch_size])
+                        b_pobs = np.array(batch[:, 0].tolist(), dtype=np.float32).reshape(batch_size, -1) # List of the observations taken from the batch
+                        b_pact = np.array(batch[:, 1].tolist(), dtype=np.int32) # List of the actions taken corresponding to each observation
+                        b_reward = np.array(batch[:, 2].tolist(), dtype=np.int32) # List of the corrsponding rewards
+                        b_obs = np.array(batch[:, 3].tolist(), dtype=np.float32).reshape(batch_size, -1) # next set of observations
+                        b_done = np.array(batch[:, 4].tolist(), dtype=np.bool)
+
+                        q = Q(b_pobs) # feed the observations in Q
+                        maxq = np.max(Q_ast(b_obs).data, axis=1) #take max output of Q
+                        target = copy.deepcopy(q.data)
+                        for j in range(batch_size):
+                            target[j, b_pact[j]] = b_reward[j]+gamma*maxq[j]*(not b_done[j]) # DQN expression
+                        Q.reset()
+                        loss = F.mean_squared_error(q, target)
+                        total_loss += loss.data
+                        loss.backward()
+                        optimizer.update()
+
+                if total_step % update_q_freq == 0:
+                    Q_ast = copy.deepcopy(Q) # Copy the learned weights from the learned nnetwork to the target network.
+
+            # epsilon
+            if epsilon > epsilon_min and total_step > start_reduce_epsilon:
+                epsilon -= epsilon_decrease
+
+            # next step
+            total_reward += reward
+            pobs = obs
+            step += 1
+            total_step += 1
+
+        total_rewards.append(total_reward)
+        total_losses.append(total_loss)
+
+        if (epoch+1) % show_log_freq == 0:
+            log_reward = sum(total_rewards[((epoch+1)-show_log_freq):])/show_log_freq
+            log_loss = sum(total_losses[((epoch+1)-show_log_freq):])/show_log_freq
+            elapsed_time = time.time()-start
+            print('\t'.join(map(str, [epoch+1, epsilon, total_step, log_reward, log_loss, elapsed_time])))
+            start = time.time()
+
+    return Q, total_losses, total_rewards
+
+Q, total_losses, total_rewards = train_dqn(Environment1(train))
+
+def plot_loss_reward(total_losses, total_rewards):
+
+    figure = tools.make_subplots(rows=1, cols=2, subplot_titles=('loss', 'reward'), print_grid=False)
+    figure.append_trace(Scatter(y=total_losses, mode='lines', line=dict(color='skyblue')), 1, 1)
+    figure.append_trace(Scatter(y=total_rewards, mode='lines', line=dict(color='orange')), 1, 2)
+    figure['layout']['xaxis1'].update(title='epoch')
+    figure['layout']['xaxis2'].update(title='epoch')
+    figure['layout'].update(height=400, width=900, showlegend=False)
+    iplot(figure)
+
+plot_loss_reward(total_losses, total_rewards)
+
+def plot_train_test_by_q(train_env, test_env, Q, algorithm_name):
+
+    # train
+    pobs = train_env.reset()
+    train_acts = []
+    train_rewards = []
+
+    for _ in range(len(train_env.data)-1):
+
+        pact = Q(np.array(pobs, dtype=np.float32).reshape(1, -1))
+        pact = np.argmax(pact.data)
+        train_acts.append(pact)
+
+        obs, reward, done = train_env.step(pact)
+        train_rewards.append(reward)
+
+        pobs = obs
+
+    train_profits = train_env.profits
+
+    # test
+    pobs = test_env.reset()
+    test_acts = []
+    test_rewards = []
+
+    for _ in range(len(test_env.data)-1):
+
+        pact = Q(np.array(pobs, dtype=np.float32).reshape(1, -1))
+        pact = np.argmax(pact.data)
+        test_acts.append(pact)
+
+        obs, reward, done = test_env.step(pact)
+        test_rewards.append(reward)
+
+        pobs = obs
+
+    test_profits = test_env.profits
+
+    # plot
+    train_copy = train_env.data.copy()
+    test_copy = test_env.data.copy()
+    train_copy['act'] = train_acts + [np.nan]
+    train_copy['reward'] = train_rewards + [np.nan]
+    test_copy['act'] = test_acts + [np.nan]
+    test_copy['reward'] = test_rewards + [np.nan]
+    train0 = train_copy[train_copy['act'] == 0]
+    train1 = train_copy[train_copy['act'] == 1]
+    train2 = train_copy[train_copy['act'] == 2]
+    test0 = test_copy[test_copy['act'] == 0]
+    test1 = test_copy[test_copy['act'] == 1]
+    test2 = test_copy[test_copy['act'] == 2]
+    act_color0, act_color1, act_color2 = 'gray', 'cyan', 'magenta'
+
+    data = [
+        Candlestick(x=train0.index, open=train0['Open'], high=train0['High'], low=train0['Low'], close=train0['Close'], increasing=dict(line=dict(color=act_color0)), decreasing=dict(line=dict(color=act_color0))),
+        Candlestick(x=train1.index, open=train1['Open'], high=train1['High'], low=train1['Low'], close=train1['Close'], increasing=dict(line=dict(color=act_color1)), decreasing=dict(line=dict(color=act_color1))),
+        Candlestick(x=train2.index, open=train2['Open'], high=train2['High'], low=train2['Low'], close=train2['Close'], increasing=dict(line=dict(color=act_color2)), decreasing=dict(line=dict(color=act_color2))),
+        Candlestick(x=test0.index, open=test0['Open'], high=test0['High'], low=test0['Low'], close=test0['Close'], increasing=dict(line=dict(color=act_color0)), decreasing=dict(line=dict(color=act_color0))),
+        Candlestick(x=test1.index, open=test1['Open'], high=test1['High'], low=test1['Low'], close=test1['Close'], increasing=dict(line=dict(color=act_color1)), decreasing=dict(line=dict(color=act_color1))),
+        Candlestick(x=test2.index, open=test2['Open'], high=test2['High'], low=test2['Low'], close=test2['Close'], increasing=dict(line=dict(color=act_color2)), decreasing=dict(line=dict(color=act_color2)))
+    ]
+    title = '{}: train s-reward {}, profits {}, test s-reward {}, profits {}'.format(
+        algorithm_name,
+        int(sum(train_rewards)),
+        int(train_profits),
+        int(sum(test_rewards)),
+        int(test_profits)
+    )
+    layout = {
+        'title': title,
+        'showlegend': False,
+         'shapes': [
+             {'x0': date_split, 'x1': date_split, 'y0': 0, 'y1': 1, 'xref': 'x', 'yref': 'paper', 'line': {'color': 'rgb(0,0,0)', 'width': 1}}
+         ],
+        'annotations': [
+            {'x': date_split, 'y': 1.0, 'xref': 'x', 'yref': 'paper', 'showarrow': False, 'xanchor': 'left', 'text': ' test data'},
+            {'x': date_split, 'y': 1.0, 'xref': 'x', 'yref': 'paper', 'showarrow': False, 'xanchor': 'right', 'text': 'train data '}
+        ]
+    }
+    figure = Figure(data=data, layout=layout)
+    iplot(figure)
+
+plot_train_test_by_q(Environment1(train), Environment1(test), Q, 'DQN')
